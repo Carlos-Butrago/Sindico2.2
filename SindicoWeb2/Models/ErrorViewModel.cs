6bc5449be27c8d09e33d01ac181eeb4ba224318e
cadu_@cebm <cadu_crazu@hotmail.com>
2020-06-24 18:32:40 -0400

    first commit

---
+++ SindicoWeb2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SindicoWeb2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
