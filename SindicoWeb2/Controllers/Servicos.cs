05929d6f42c053b3802505fc9887098a188ce842
Carlos Butrago <carlos.butrago@gestaoprojetos.inf.br>
2020-08-27 01:47:02 -0300

    atualizando para teste

---
+++ SindicoWeb2/Controllers/Servicos.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SindicoWeb2.Models;
+
+namespace SindicoWeb2.Controllers
+{
+    public class Servicos : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public Servicos(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Gestao()
+        {
+            return View();
+        }
+
+        public IActionResult Outorga()
+        {
+            return View();
+        }
+
+        public IActionResult Coleta()
+        {
+            return View();
+        }
+           public IActionResult Tratamento()
+        {
+            return View();
+        }
+              
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
